3c4b483193fce44a4d7ed9df9750e09f26742a48
“Magd <majdgalyl@gmail.com>
2021-12-01 19:38:51 +0200

    Finished season app config (react & JS)

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,41 @@
 import React from "react";
 
-const SeasonDisplay = () => {
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the sun",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "Burr, its chilly",
+        iconName: 'snowflake'
+    }
+}
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month <9) {
+       return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ?  'winter' :'summer';
+    }
+}
+
+const SeasonDisplay = (props) => {
+    // const text = season === 'winter' ? 'Burr, its chilly' : 'Lets hit the beach';
+    // const icon = season === 'winter' ? 'snowflake' : 'Lets hit the sun';
+    // console.log(season);
+
+    // bellow season variable will get us user season
+    const season = getSeason(props.lat, new Date().getMonth());
+    const { text, iconName } = seasonConfig[season];
+    // then using seasonConfig[season we got from variable]
+    // we destructred the config objects and used its keys bellow
+
     return(
-        <div>SeasonDisplay</div>
+        <div>
+            <i className = { `${iconName} massive icon`} ></i>
+            <h1>{ text }</h1>
+            <i className = { `${iconName} massive icon`} ></i>
+        </div>
     );
 };
 
